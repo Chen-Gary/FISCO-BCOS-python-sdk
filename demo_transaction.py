b5cff592b87aa0b609749e024d32913ea3f4e7a7
Gary Chen <chenhhgary@gmail.com>
2021-10-02 11:55:03 +0800

    Update demo_transaction.py

--- demo_transaction.py
+++ demo_transaction.py
@@ -23,10 +23,11 @@ from client_config import client_config
 import sys
 import traceback
 # 从文件加载abi定义
-if os.path.isfile(client_config.solc_path) or os.path.isfile(client_config.solcjs_path):
-    Compiler.compile_file("contracts/HelloWorld.sol")
-    Compiler.compile_file("contracts/SimpleInfo.sol")
-abi_file = "contracts/SimpleInfo.abi"
+# if os.path.isfile(client_config.solc_path) or os.path.isfile(client_config.solcjs_path):
+#     Compiler.compile_file("contracts/HelloWorld.sol")
+#     Compiler.compile_file("contracts/SimpleInfo.sol")
+abi_file = "contracts/HelloWorld.abi"
+#abi_file = "contracts/SimpleInfo.abi"
 data_parser = DatatypeParser()
 data_parser.load_abi_file(abi_file)
 contract_abi = data_parser.contract_abi
@@ -34,59 +35,69 @@ contract_abi = data_parser.contract_abi
 try:
     client = BcosClient()
     print(client.getinfo())
-    # 部署合约
-    print("\n>>Deploy:----------------------------------------------------------")
-    with open("contracts/SimpleInfo.bin", 'r') as load_f:
-        contract_bin = load_f.read()
-        load_f.close()
-    result = client.deploy(contract_bin)
-    print("deploy", result)
-    print("new address : ", result["contractAddress"])
-    contract_name = os.path.splitext(os.path.basename(abi_file))[0]
-    memo = "tx:" + result["transactionHash"]
-    # 把部署结果存入文件备查
-    ContractNote.save_address_to_contract_note(contract_name,
-                                               result["contractAddress"])
-    # 发送交易，调用一个改写数据的接口
-    print("\n>>sendRawTransaction:----------------------------------------------------")
-    to_address = result['contractAddress']  # use new deploy address
-    args = ['simplename', 2024, to_checksum_address('0x7029c502b4F824d19Bd7921E9cb74Ef92392FB1c')]
 
+    # # 部署合约
+    # print("\n>>Deploy:----------------------------------------------------------")
+    # with open("contracts/SimpleInfo.bin", 'r') as load_f:
+    #     contract_bin = load_f.read()
+    #     load_f.close()
+    # result = client.deploy(contract_bin)
+    # print("deploy", result)
+    # print("new address : ", result["contractAddress"])
+    # contract_name = os.path.splitext(os.path.basename(abi_file))[0]
+    # memo = "tx:" + result["transactionHash"]
+    # # 把部署结果存入文件备查
+    # ContractNote.save_address_to_contract_note(contract_name,
+    #                                            result["contractAddress"])
+
+    # # 发送交易，调用一个改写数据的接口
+    # print("\n>>sendRawTransaction:----------------------------------------------------")
+    # to_address = result['contractAddress']  # use new deploy address
+    # args = ['simplename', 2024, to_checksum_address('0x7029c502b4F824d19Bd7921E9cb74Ef92392FB1c')]
+
+    # receipt = client.sendRawTransactionGetReceipt(to_address, contract_abi, "set", args)
+    # print("receipt:", receipt)
+
+    # # 解析receipt里的log
+    # print("\n>>parse receipt and transaction:--------------------------------------")
+    # txhash = receipt['transactionHash']
+    # print("transaction hash: ", txhash)
+    # logresult = data_parser.parse_event_logs(receipt["logs"])
+    # i = 0
+    # for log in logresult:
+    #     if 'eventname' in log:
+    #         i = i + 1
+    #         print("{}): log name: {} , data: {}".format(i, log['eventname'], log['eventdata']))
+    # # 获取对应的交易数据，解析出调用方法名和参数
+
+    # txresponse = client.getTransactionByHash(txhash)
+    # inputresult = data_parser.parse_transaction_input(txresponse['input'])
+    # print("transaction input parse:", txhash)
+    # print(inputresult)
+
+    # # 解析该交易在receipt里输出的output,即交易调用的方法的return值
+    # outputresult = data_parser.parse_receipt_output(inputresult['name'], receipt['output'])
+    # print("receipt output :", outputresult)
+
+    # 调用一下call，获取数据
+    to_address = "0x60b364cab35c19678c6fa3d4a6cdf705a2adea89"
+    print("\n>>Call:------------------------------------------------------------------------")
+    res = client.call(to_address, contract_abi, "get")
+    print("call getbalance result:", res)
+    # res = client.call(to_address, contract_abi, "getbalance1", [100])
+    # print("call getbalance1 result:", res)
+    # res = client.call(to_address, contract_abi, "getname")
+    # print("call getname:", res)
+    # res = client.call(to_address, contract_abi, "getall")
+    # print("call getall result:", res)
+    print("done,demo_tx,total req {}".format(client.request_counter))
+
+    args = ['hello world 1002']
     receipt = client.sendRawTransactionGetReceipt(to_address, contract_abi, "set", args)
     print("receipt:", receipt)
 
-    # 解析receipt里的log
-    print("\n>>parse receipt and transaction:--------------------------------------")
-    txhash = receipt['transactionHash']
-    print("transaction hash: ", txhash)
-    logresult = data_parser.parse_event_logs(receipt["logs"])
-    i = 0
-    for log in logresult:
-        if 'eventname' in log:
-            i = i + 1
-            print("{}): log name: {} , data: {}".format(i, log['eventname'], log['eventdata']))
-    # 获取对应的交易数据，解析出调用方法名和参数
-
-    txresponse = client.getTransactionByHash(txhash)
-    inputresult = data_parser.parse_transaction_input(txresponse['input'])
-    print("transaction input parse:", txhash)
-    print(inputresult)
-
-    # 解析该交易在receipt里输出的output,即交易调用的方法的return值
-    outputresult = data_parser.parse_receipt_output(inputresult['name'], receipt['output'])
-    print("receipt output :", outputresult)
-
-    # 调用一下call，获取数据
-    print("\n>>Call:------------------------------------------------------------------------")
-    res = client.call(to_address, contract_abi, "getbalance")
+    res = client.call(to_address, contract_abi, "get")
     print("call getbalance result:", res)
-    res = client.call(to_address, contract_abi, "getbalance1", [100])
-    print("call getbalance1 result:", res)
-    res = client.call(to_address, contract_abi, "getname")
-    print("call getname:", res)
-    res = client.call(to_address, contract_abi, "getall")
-    print("call getall result:", res)
-    print("done,demo_tx,total req {}".format(client.request_counter))
 
 except BcosException as e:
     print("execute demo_transaction failed ,BcosException for: {}".format(e))
